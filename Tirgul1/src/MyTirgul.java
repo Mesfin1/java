13b4d491cc3e85c823c639ea057123f4e8157a17
Student <Student@10.0.10.117>
2016-03-01 16:40:28 +0200

    second syso

--- Tirgul1/src/MyTirgul.java
+++ Tirgul1/src/MyTirgul.java
@@ -3,7 +3,8 @@ public class MyTirgul {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.err.println("my tirgul");
+       System.err.println("my tirgul");
+       System.out.println("qqqqqq");
 	}
 
 }
